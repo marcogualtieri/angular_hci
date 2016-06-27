b9ff9bc00e18f57ea00d6e60d3efd0cc8b2ac526
Marco Gualtieri <marco.gualtieri@fleetmatics.com>
2016-06-27 23:13:32 +0200

    refactor of data generation

--- js/controllers.js
+++ js/controllers.js
@@ -1,33 +1,7 @@
-var WorkOrderStatuses = {
-    OPEN: 0,
-    COMPLETED: 1,
-    FAILED: 2
-};
-
-function WorkOrder(sequence, name, description, lat, lng, service) {
-    this.sequence = sequence;
-    this.name = name;
-    this.description = description;
-    this.service = service;
-    this.lat = lat;
-    this.lng = lng;
-    this.status = WorkOrderStatuses.OPEN;
-}
-
-var workOrders = [
-    new WorkOrder(1, "Aviva Stadium", "", 53.335232, -6.228167, 30),
-    new WorkOrder(2, "St Patrick", "", 53.339590, -6.272441, 30),
-    new WorkOrder(3, "Trinity College", "", 53.344587, -6.259389, 30),
-    new WorkOrder(4, "Guinnes Storehouse", "", 53.341874, -6.286725, 30),
-    new WorkOrder(5, "Jameson Distillery", "", 53.348463, -6.277975, 30),
-    new WorkOrder(6, "Croke Park", "", 53.360963, -6.252282, 30),
-    new WorkOrder(7, "National Post Office", "", 53.349450, -6.260210, 30),
-    new WorkOrder(8, "Dublin Port", "", 53.349509, -6.207904, 30)
-];
 
 var app = angular.module('app', ['ui.bootstrap', 'ngTouch']);
 
-app.controller('modalInstanceController', function ($scope, $rootScope, $uibModalInstance) {
+app.controller('orientationModalController', function ($scope, $rootScope, $uibModalInstance) {
     $scope.dismiss = function() {
         $uibModalInstance.dismiss('cancel');
         $rootScope.orientationModalOpened = false;
@@ -50,7 +24,7 @@ app.controller('orientationController', function ($scope, $rootScope, $window, $
                 animation: true,
                 size: 'sm',
                 templateUrl: 'orientationModal.html',
-                controller: 'modalInstanceController'
+                controller: 'orientationModalController'
             });
             $rootScope.orientationModalOpened = true;
         }
@@ -60,9 +34,10 @@ app.controller('orientationController', function ($scope, $rootScope, $window, $
 
 app.controller('appController', function ($scope) {
 
-    var mapContainer = document.getElementById('map');
+    // map 
+
     $scope.map = new google.maps.Map(
-        mapContainer, 
+        document.getElementById('map'), 
         {
             zoom: 4,
             center: new google.maps.LatLng(40.0000, -98.0000),
@@ -73,40 +48,57 @@ app.controller('appController', function ($scope) {
             } 
         }
     );
-    $scope.markers = [];
+
+    // map info window
+
     var infoWindow = new google.maps.InfoWindow();
 
-    var createMarker = function (workOrder) {
-        var marker = new google.maps.Marker({
-            map: $scope.map,
-            icon: {
-                url: 'http://maps.google.com/mapfiles/ms/icons/blue.png',
-                origin: new google.maps.Point(0, -5)
-            },
-            position: new google.maps.LatLng(workOrder.lat, workOrder.lng),
-            title: workOrder.name,
-            label: workOrder.sequence.toString(),
-            sequence: workOrder.sequence
-        });
-        marker.content = '<a href="geo:' + workOrder.lat + ',' + workOrder.lng + '" target="_blank">Direction</a>' 
-            + ' <a href="tel:+12345678">Phone</a>';
-        google.maps.event.addListener(marker, 'click', function (e) {
-            infoWindow.setContent(marker.title + '<br>' + marker.content);
+    var setEventForMarkerClick = function (marker) {
+        google.maps.event.addListener(marker, 'click', function (evt) {
+            infoWindow.setContent(marker.content);
             infoWindow.open($scope.map, marker);
             $scope.map.setCenter(marker.getPosition());
         });
-        $scope.markers.push(marker);
-    }  
+    };
+
+    var generateMarkersForWorkOrders = function (workOrders) {
+        for (var i = 0; i < workOrders.length; i++) {
+            var workOrder = workOrders[i];
+            var marker = new google.maps.Marker({
+                map: $scope.map,
+                icon: {
+                    url: 'http://maps.google.com/mapfiles/ms/icons/blue.png',
+                    origin: new google.maps.Point(0, -5)
+                },
+                position: new google.maps.LatLng(workOrder.lat, workOrder.lng),
+                title: workOrder.name,
+                label: workOrder.index.toString(),
+                index: workOrder.index
+            });
+            marker.content = marker.title + '<br>' + '<a href="geo:' + workOrder.lat + ',' + workOrder.lng + '" target="_blank">Direction</a>' 
+                + ' <a href="tel:+12345678">Phone</a>';    
+            workOrders[i].marker = marker;
+            setEventForMarkerClick(workOrders[i].marker);
+        }
+    };
+
+    // get work orders
+
+    $scope.workOrders = DataProvider.WorkOrders;
     
-    for (i = 0; i < workOrders.length; i++) {
-        createMarker(workOrders[i]);
-    }
+    generateMarkersForWorkOrders($scope.workOrders);
+
+    console.log($scope.workOrders);
 
     centerMap();
 
-    $scope.openInfoWindow = function (e, selectedMarker) {
-        e.preventDefault();
-        google.maps.event.trigger(selectedMarker, 'click');
+    $scope.selectedIndex = null;
+
+    // questo deve ricevere il workOrder, che deve avere come property il marker
+    // così posso accedere al sequence id e aggiornare l'active li
+    $scope.openInfoWindow = function (evt, selectedWorkOrder) {
+        evt.preventDefault();
+        google.maps.event.trigger(selectedWorkOrder.marker, 'click');
     }
 
     google.maps.event.addListener($scope.map, 'click', function (e) {
@@ -126,8 +118,8 @@ app.controller('appController', function ($scope) {
 
     function centerMap() {
         var bounds = new google.maps.LatLngBounds();
-        for (var i = 0; i < $scope.markers.length; i++) {
-            bounds.extend($scope.markers[i].getPosition());
+        for (var i = 0; i < $scope.workOrders.length; i++) {
+            bounds.extend($scope.workOrders[i].marker.getPosition());
         }   
         $scope.map.fitBounds(bounds);
     }
--- js/mock_data_provider.js
+++ js/mock_data_provider.js
@@ -1,18 +1,46 @@
-var WorkOrderStatuses = {
-    OPEN: 0,
-    COMPLETED: 1,
-    FAILED: 2
-};
+DataProvider = (function() {
 
-function WorkOrder(name, lat, lng, service) {
-    this.name = name;
-    this.service = service;
-    this.lat = lat;
-    this.lng = lng;
-    this.status = WorkOrderStatuses.OPEN;
-}
+    var workOrderStatuses = {
+        TODO: 0,
+        DONE: 1,
+        FAIL: 2
+    };
+    
+    var WorkOrder = function(index, lat, lng, name, description, service) {
+        this.index = index;
+        this.lat = lat;
+        this.lng = lng;
+        this.name = name;
+        this.description = description;
+        this.service = service;
+        this.status = workOrderStatuses.TODO;
+        this.failureDetails = "";
+        this.setTodo = function() {
+            this.status = workOrderStatuses.TODO;
+        };
+        this.setDone = function() {
+            this.status = workOrderStatuses.DONE;
+        };
+        this.setFail = function(failureDetails) {
+            this.status = workOrderStatuses.FAIL;
+            this.failureDetails = failureDetails;
+        };
+    };
 
-var workOrders = [
-    new WorkOrder("a", 43, 11.1, 30),
-    new WorkOrder("b", 43, 11.12, 30)
-];
+    var workOrders = [
+        new WorkOrder(1, 53.335232, -6.228167, "Aviva Stadium", "Delivery", 10),
+        new WorkOrder(2, 53.339590, -6.272441, "St Patrick", "Maintenance", 60),
+        new WorkOrder(3, 53.344587, -6.259389, "Trinity College", "Delivery", 40),
+        new WorkOrder(4, 53.341874, -6.286725, "Guinnes Storehouse", "Repair", 50),
+        new WorkOrder(5, 53.348463, -6.277975, "Jameson Distillery", "Delivery", 10),
+        new WorkOrder(6, 53.360963, -6.252282, "Croke Park", "Pickup", 20),
+        new WorkOrder(7, 53.349450, -6.260210, "National Post Office", "Pickup", 25),
+        new WorkOrder(8, 53.349509, -6.207904, "Dublin Port", "Repair", 30)
+    ];
+
+    return {
+        WorkOrders: workOrders,
+        WorkOrderStatuses: workOrderStatuses
+    };
+
+}());
